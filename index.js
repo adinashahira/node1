fc03e25036ac137fd33961f63ffef25895c071b2
EC2 Default User <ec2-user@ip-172-31-45-54.ap-southeast-1.compute.internal>
2020-10-22 03:17:08 +0000

    changed index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 9000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!ff')
+  res.send('Hello World from PuTTY shippableb branch')
 });
 
 app.listen(port, () => {
