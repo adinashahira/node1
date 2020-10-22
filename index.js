237c083e743dc3773302ed2371daa56455ddcca3
EC2 Default User <ec2-user@ip-172-31-45-54.ap-southeast-1.compute.internal>
2020-10-22 12:11:29 +0000

    show port

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-//const express = require('express')
-//const app = express();
-//const port = 8000;
+const express = require('express')
+const app = express();
+const port = 8000;
 
-/*app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World! from Putty')
-});*/
+})
 
-//app.listen(port, () => {
+app.listen(port, () => {
  console.log(`Example app listening on port ${port}!`)
  // console.log('Hello World from PuTTY master branch')
-//});
+});
 
 /*setTimeout((function() {
     return process.exit(1);
