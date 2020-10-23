0375fcc0585ba493306705b82242032074929baf
EC2 Default User <ec2-user@ip-172-31-32-254.ap-southeast-1.compute.internal>
2020-10-23 08:14:02 +0000

    q

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3100;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from VSCode test1')
+  res.send('Hello World! from VSCode test2')
 })
 
 app.listen(port, () => {
